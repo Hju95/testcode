d36d268e941036b93f64b93ddbd5bb6d991e493c
wise_JG <59231743+Hju95@users.noreply.github.com>
2023-12-19 01:40:12 +0900

    Business Layer 테스트(1) 마지막 에러 해결

--- src/main/java/sample/cafekiosk/spring/domain/order/Order.java
+++ src/main/java/sample/cafekiosk/spring/domain/order/Order.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import sample.cafekiosk.spring.domain.orderproduct.OrderProduct;
 import sample.cafekiosk.spring.domain.product.Product;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,22 +32,22 @@ public class Order {
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderProduct> orderProducts = new ArrayList<>();
 
-    public Order(List<Product> products, LocalDateTime regiseredDateTime) {
-        this.orderStatus = OrderStatus.INIT;
-        this.totalPrice = calculateTotalPrice(products);
-        this.registeredDateTime = regiseredDateTime;
-    }
-
-
-
 //    public Order(List<Product> products, LocalDateTime registeredDateTime) {
 //        this.orderStatus = OrderStatus.INIT;
 //        this.totalPrice = calculateTotalPrice(products);
 //        this.registeredDateTime = registeredDateTime;
-//        this.orderProducts = products.stream()
-//                .map(product -> new OrderProduct(this, product))
-//                .collect(Collectors.toList());
 //    }
+
+
+
+    public Order(List<Product> products, LocalDateTime registeredDateTime) {
+        this.orderStatus = OrderStatus.INIT;
+        this.totalPrice = calculateTotalPrice(products);
+        this.registeredDateTime = registeredDateTime;
+        this.orderProducts = products.stream()
+                .map(product -> new OrderProduct(this, product))
+                .collect(Collectors.toList());
+    }
 
     public static Order create(List<Product> products, LocalDateTime registeredDateTime) {
         return new Order(products, registeredDateTime);
